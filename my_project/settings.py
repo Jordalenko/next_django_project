efcc0b25c9f624a24eff432b6b71ca589f5b25fa
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-04-20 12:10:15 +0100

    correct installed apps in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.hello_world',
-    'django.contrib.about',
+    'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
